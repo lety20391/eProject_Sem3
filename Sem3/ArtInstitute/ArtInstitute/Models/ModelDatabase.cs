32523cb73d78cc6be4e5c6a6e967754a749dce87
lety20391 <lety_20391@yahoo.com>
2018-10-26 18:47:42 +0700

    Entity Framework & Model Database
    install Entity Framework and Model Database

---
+++ Sem3/ArtInstitute/ArtInstitute/Models/ModelDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ArtInstitute.Models;
+
+namespace ArtInstitute.Models
+{
+    public class ModelDatabase : DbContext
+    {
+        public ModelDatabase() : base("CNString") { }
+        public ModelDatabase(string connectionString) : base(connectionString) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Award> tbAward { get; set; }
+        public DbSet<Comment> tbComment { get; set; }
+        public DbSet<Competition> tbCompetition { get; set; }
+        public DbSet<Customer> tbCustomer { get; set; }
+        public DbSet<Exam> tbExam { get; set; }
+        public DbSet<Exhibition> tbExhibition { get; set; }
+        public DbSet<Manager> tbManager { get; set; }
+        public DbSet<Staff> tbStaff { get; set; }
+        public DbSet<Student> tbStudent { get; set; }
+        public DbSet<User> tbUser { get; set; }
+
+    }
+}
