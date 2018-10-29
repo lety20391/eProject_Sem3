6b6974320ac69ae76103318afdf01dd182f26191
ELISA <TRANTHITUYETDUYEN@YAHOO.COM>
2018-10-29 16:38:31 +0700

    exhibitioncontroller

---
+++ MainProject3/MainProject3/Controllers/ExhibitionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MainProject3.Models;
+
+namespace MainProject3.Controllers
+{
+    public class ExhibitionsController : Controller
+    {
+        private ModelMain db = new ModelMain();
+
+        // GET: Exhibitions
+        public ActionResult Index()
+        {
+            return View(db.Exhibitions.ToList());
+        }
+
+        // GET: Exhibitions/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exhibition exhibition = db.Exhibitions.Find(id);
+            if (exhibition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exhibition);
+        }
+
+        // GET: Exhibitions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Exhibitions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ExhibitionID,ID,Detail,Country,StartDate,EndDate,Condition,Quantity")] Exhibition exhibition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Exhibitions.Add(exhibition);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(exhibition);
+        }
+
+        // GET: Exhibitions/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exhibition exhibition = db.Exhibitions.Find(id);
+            if (exhibition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exhibition);
+        }
+
+        // POST: Exhibitions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ExhibitionID,ID,Detail,Country,StartDate,EndDate,Condition,Quantity")] Exhibition exhibition)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(exhibition).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(exhibition);
+        }
+
+        // GET: Exhibitions/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Exhibition exhibition = db.Exhibitions.Find(id);
+            if (exhibition == null)
+            {
+                return HttpNotFound();
+            }
+            return View(exhibition);
+        }
+
+        // POST: Exhibitions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Exhibition exhibition = db.Exhibitions.Find(id);
+            db.Exhibitions.Remove(exhibition);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
