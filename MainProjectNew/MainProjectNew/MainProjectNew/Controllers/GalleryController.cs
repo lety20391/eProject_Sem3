26dd88be743dd0fb8d624f8dcc63701d54f78e08
lety20391 <lety_20391@yahoo.com>
2018-11-02 10:41:54 +0700

    add Gallery
    add Gallery

--- MainProjectNew/MainProjectNew/MainProjectNew/Controllers/GalleryController.cs
+++ MainProjectNew/MainProjectNew/MainProjectNew/Controllers/GalleryController.cs
@@ -23,8 +23,15 @@ namespace MainProjectNew.Controllers
             return View(db.Exams.Where(item => item.IDExhibition.Equals(id)).ToList());
         }
 
-        public ActionResult buyPhoto(string id)
+        public ActionResult buyPhoto(string id, string exhiID)
         {
+            ViewBag.ExamID = id;
+            ViewBag.ExhibitionID = exhiID;
+
+            System.Diagnostics.Debug.WriteLine("---------------------------------");
+            System.Diagnostics.Debug.WriteLine(id);
+            System.Diagnostics.Debug.WriteLine(exhiID);
+            System.Diagnostics.Debug.WriteLine("---------------------------------");
             return View();
         }
     }
