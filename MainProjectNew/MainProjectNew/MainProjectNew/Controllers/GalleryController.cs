50771518f556fdc456dc89b569404bf90f5147b0
lety20391 <lety_20391@yahoo.com>
2018-11-02 08:12:02 +0700

    add simple Gallery
    at /Gallery/getGallery/{Exhibition ID}

--- MainProjectNew/MainProjectNew/MainProjectNew/Controllers/GalleryController.cs
+++ MainProjectNew/MainProjectNew/MainProjectNew/Controllers/GalleryController.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MainProjectNew.Models;
 
 namespace MainProjectNew.Controllers
 {
     public class GalleryController : Controller
     {
         // GET: Gallery
+        ModelMain db = new ModelMain();
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getGallery(string id)
+        {
+            //List<String> listImg = db.Exams.Where(item => item.IDExhibition.Equals(id)).Select(item => item.Path).Take(9).ToList();
+
+            return View(db.Exams.Where(item => item.IDExhibition.Equals(id)).ToList());
+        }
+
+        public ActionResult buyPhoto(string id)
         {
             return View();
         }
